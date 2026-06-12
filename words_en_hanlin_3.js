e4b3736c745a41b2c03b80de508b797fb7382dc1
TVIMAX <eyestaiwan@gmail.com>
2026-06-13 04:33:07 +0800

    Add files via upload

---
+++ words_en_hanlin_3.js
@@ -0,0 +1,93 @@
+// 確保全域變數存在，以便合併多冊字庫
+if (typeof window.englishDb === 'undefined') { window.englishDb = {}; }
+if (!window.englishDb["翰林"]) { window.englishDb["翰林"] = {}; }
+
+// ==========================================
+// 翰林版 3下 (Book 6) 完整字庫 (全單字 + 完整中文翻譯例句)
+// ==========================================
+window.englishDb["翰林"]["三下"] = {
+    "Lesson 1": [
+        { word: "either", translate: "也(不)", sentence: "Meg doesn't like shopping, and her sister doesn't, either. (梅格不喜歡購物，她妹妹也不喜歡。)" },
+        { word: "north", translate: "向北地;北方(的)", sentence: "The north side of the house is facing a beautiful garden. (房子的北側正對著一個美麗的花園。)" },
+        { word: "south", translate: "向南地;南方(的)", sentence: "Some birds fly south in winter to look for food. (有些鳥兒在冬天會往南飛去尋找食物。)" },
+        { word: "east", translate: "向東地;東方(的)", sentence: "The sun rises in the east every morning. (太陽每天早上從東方升起。)" },
+        { word: "west", translate: "向西地;西方(的)", sentence: "They plan to go to the temple in the west of the city. (他們計畫去城市西邊的寺廟。)" },
+        { word: "get married", translate: "結婚", sentence: "Jim and Ella got married and had their first baby a year later. (吉姆和艾拉結婚了，並在一年後生了他們的第一個寶寶。)" },
+        { word: "neither", translate: "也不", sentence: "Peter wasn't invited to the party, and neither were his brothers. (彼得沒有被邀請參加派對，他的兄弟們也沒有。)" },
+        { word: "shy", translate: "害羞的", sentence: "Hank is too shy to ask Michelle out for dinner. (漢克太害羞了，不敢約蜜雪兒出去吃晚餐。)" },
+        { word: "lonely", translate: "寂寞的;孤獨的", sentence: "Lily feels lonely at school because she has no one to talk to. (莉莉在學校感到孤獨，因為沒有人可以跟她說話。)" },
+        { word: "deal", translate: "約定;交易", sentence: "A: I will treat you to lunch if you win the singing contest. B: OK. It's a deal. (A: 如果你贏得歌唱比賽，我就請你吃午餐。B: 好的。一言為定。)" },
+        { word: "date", translate: "約會", sentence: "On their first date, Judy took Greg to a nice restaurant by the beach. (在他們第一次約會時，茱蒂帶葛瑞格去了海邊一家不錯的餐廳。)" },
+        { word: "cheer up", translate: "振作起來", sentence: "Cheer up! Failing an exam is not the end of the world. (振作起來！考試不及格不是世界末日。)" },
+        { word: "even though", translate: "即使", sentence: "Even though Tina studied hard, she still did poorly on the science test. (即使蒂娜很努力讀書，她的自然科學考試還是考得很差。)" },
+        { word: "head", translate: "朝...行進", sentence: "If we keep heading north, we will reach the castle in ten minutes. (如果我們繼續向北行進，十分鐘後就會到達城堡。)" },
+        { word: "keep in touch", translate: "保持聯繫", sentence: "My mom and her high school friends still keep in touch with each other. (我媽媽和她的高中朋友們仍然保持聯繫。)" },
+        { word: "class leader", translate: "班長", sentence: "Lucy wants to be the class leader. (露西想當班長。)" },
+        { word: "queen", translate: "皇后", sentence: "Queen Elizabeth II was loved by the people in her country. (伊莉莎白女王二世受到她國家人民的愛戴。)" },
+        { word: "national", translate: "國家的", sentence: "January 1 is a national holiday in many countries. (一月一日在許多國家是國定假日。)" },
+        { word: "row", translate: "(一)排;行;列", sentence: "The fans stood in a row and waited their turn to meet the singer. (粉絲們站成一排，等著輪到他們見那位歌手。)" },
+        { word: "grade", translate: "年級", sentence: "Allen will be in the fifth grade after the summer. (艾倫過完夏天後就要升五年級了。)" },
+        { word: "miss", translate: "趕不上;錯過", sentence: "Because of the terrible traffic, we missed our train. (因為糟糕的交通，我們錯過了火車。)" },
+        { word: "hot spring", translate: "溫泉", sentence: "It is a pleasure to go to a hot spring on a cold winter day. (在寒冷的冬日去泡溫泉是一件樂事。)" },
+        { word: "quite", translate: "相當", sentence: "The soup may smell bad, but it tastes quite good, actually. (這湯聞起來可能很糟，但實際上嚐起來相當不錯。)" },
+        { word: "correct", translate: "更正;正確的", sentence: "My teacher corrected a few spelling mistakes in my writing. (我的老師更正了我寫作中的幾個拼字錯誤。)" },
+        { word: "power", translate: "力量", sentence: "Everyone has the power to change their lives. (每個人都有改變自己生活的力量。)" },
+        { word: "by", translate: "早於;不遲於", sentence: "We need to finish the report by this Thursday, or the teacher will be unhappy. (我們必須在這個星期四前完成報告，否則老師會不高興。)" },
+        { word: "as soon as", translate: "...就", sentence: "As soon as I got home, it started to rain. (我一到家，就開始下雨了。)" },
+        { word: "course", translate: "課程;路線", sentence: "Do you want to sign up for the English course with me? (你想和我一起報名英文課程嗎？)" },
+        { word: "part-time", translate: "兼職的", sentence: "Some college students have a part-time job after school. (有些大學生放學後有兼職工作。)" },
+        { word: "test the water", translate: "試探", sentence: "Before Peter opened his first bakery, he had many people try the bread he made to test the water. (在彼得開他的第一家麵包店之前，他讓許多人試吃他做的麵包來試探水溫。)" },
+        { word: "act on", translate: "執行;落實", sentence: "Mr. Hogan is acting on his career plan. (霍根先生正在執行他的職涯計畫。)" }
+    ],
+    "Lesson 2": [
+        { word: "blanket", translate: "毯子", sentence: "Put this thick blanket over you to stay warm. (把這條厚毯子蓋在身上保暖。)" },
+        { word: "neither... nor...", translate: "既不...也不...", sentence: "Neither Lauren nor John knows what happened to their daughter. (蘿倫和約翰都不知道他們的女兒發生了什麼事。)" },
+        { word: "stupid", translate: "愚蠢的", sentence: "The idea sounded stupid, but it actually worked. (這個想法聽起來很愚蠢，但實際上很有效。)" },
+        { word: "either... or...", translate: "不是...就是...", sentence: "Dan is either at the bookstore or at the library. (丹不是在書店就是在圖書館。)" },
+        { word: "have nothing to do with...", translate: "和...無關", sentence: "Her death was an accident. It had nothing to do with you. (她的死是一場意外。這和妳一點關係也沒有。)" },
+        { word: "mind", translate: "介意", sentence: "A: It's cold in here. Do you mind if I close the window? B: No. Please do. (A: 這裡面很冷。你介意我關上窗戶嗎？B: 不介意。請關吧。)" },
+        { word: "study", translate: "研究", sentence: "They are working on a study about plants. (他們正在進行一項關於植物的研究。)" },
+        { word: "not only... but also...", translate: "不但...也...", sentence: "Not only Fred but also Ann plays badminton well. (不但弗雷德，安的羽毛球也打得很好。)" },
+        { word: "sign", translate: "跡象", sentence: "The signs show that people used to live in this area. (這些跡象顯示過去有人住在這個地區。)" },
+        { word: "park", translate: "停車", sentence: "Look at the red line. It means we shouldn't park here. (看那條紅線。意思是我們不應該在這裡停車。)" },
+        { word: "stay up", translate: "熬夜", sentence: "Jess stayed up all night to prepare for the test. (潔絲熬夜一整晚準備考試。)" },
+        { word: "country", translate: "鄉村", sentence: "My mom loves nature, and she always dreams of living in the country one day. (我媽媽熱愛大自然，她總是夢想有一天能住在鄉村。)" },
+        { word: "sight", translate: "視線;景象", sentence: "Don't let your kids out of your sight when they are in public places. (當孩子在公共場所時，不要讓他們離開你的視線。)" },
+        { word: "leave", translate: "遺留", sentence: "The man left a note on the table before he went out. (那個男人出門前在桌上留了一張紙條。)" },
+        { word: "tidy", translate: "整齊的;整理", sentence: "I keep my room clean and tidy. (我保持我的房間乾淨整齊。)" },
+        { word: "item", translate: "品項", sentence: "What is the last item on the list? (清單上的最後一個品項是什麼？)" },
+        { word: "repeat", translate: "重複", sentence: "Could you repeat what you just said? (你能重複你剛才說的話嗎？)" },
+        { word: "package", translate: "包裹", sentence: "I ordered a book online, and it came in a big package. (我在網路上訂了一本書，它裝在一個大包裹裡送來了。)" },
+        { word: "language", translate: "語言", sentence: "How many languages do you speak? (你會說幾種語言？)" },
+        { word: "wave", translate: "揮手;揮舞", sentence: "She waved goodbye to us and then got on the train. (她向我們揮手道別，然後上了火車。)" },
+        { word: "to put it simply", translate: "簡單地說", sentence: "To put it simply, I don't think you worked hard enough. (簡單地說，我認為你不夠努力。)" },
+        { word: "store", translate: "儲藏", sentence: "I store all my pictures on the computer. (我把我所有的照片儲存在電腦裡。)" },
+        { word: "letter", translate: "字母", sentence: "How many words begin with the letter X? (有多少個字是以字母 X 開頭的？)" },
+        { word: "set", translate: "(一)套;組;副", sentence: "There are two sets of game cards on the coffee table. (咖啡桌上有兩副遊戲卡牌。)" },
+        { word: "yard", translate: "碼", sentence: "There are about 91 centimeters in a yard. (一碼大約有91公分。)" },
+        { word: "count on", translate: "依靠;指望", sentence: "Todd's family counts on him to make money. (陶德的家人指望他賺錢。)" }
+    ],
+    "Lesson 3": [
+        { word: "while", translate: "當...時", sentence: "While Mom was tidying up the house, someone called her. (當媽媽在整理房子的時候，有人打電話給她。)" },
+        { word: "point out", translate: "指出", sentence: "Jessie pointed out how dangerous it was to swim in the sea when the weather was bad. (潔西指出了天氣不好時在海裡游泳有多危險。)" },
+        { word: "please", translate: "(使)滿意;取悦", sentence: "It's not possible to please everyone. (要取悅每個人是不可能的。)" },
+        { word: "case", translate: "案例", sentence: "The lawyer is happy because she has just won a case. (那位律師很高興，因為她剛贏了一個案子。)" },
+        { word: "point", translate: "意義;論點", sentence: "Tony made a good point in his report. (東尼在他的報告中提出了一個很好的論點。)" }
+    ],
+    "Lesson 4": [
+        { word: "vote", translate: "投票", sentence: "I voted for Peter as our class leader because he likes helping people. (我投票給彼得當班長，因為他喜歡幫助人。)" },
+        { word: "election", translate: "選舉", sentence: "There is an important election in this country every four years. (這個國家每四年有一場重要的選舉。)" },
+        { word: "check in", translate: "報到", sentence: "She checked in at a hotel after a long day of travel. (在漫長的一天旅行後，她在飯店辦理了入住手續。)" },
+        { word: "leave for", translate: "前往", sentence: "We had a party for John before he left for the USA to study. (在約翰前往美國讀書前，我們為他辦了一場派對。)" },
+        { word: "stamp", translate: "印章;蓋章", sentence: "The stamp is so old that I can't read the words on it. (這個印章太舊了，我看不清上面的字。)" },
+        { word: "following", translate: "以下(的)", sentence: "Please bring the following items to the voting place: your ID card, name stamp, and voter notice. (請攜帶以下物品前往投票所：身分證、印章和投票通知單。)" },
+        { word: "raise", translate: "舉起;提高", sentence: "Raise your hand if you have any questions. (如果有任何問題請舉手。)" },
+        { word: "deal with", translate: "處理;負責", sentence: "We have to deal with the problem carefully. (我們必須小心地處理這個問題。)" }
+    ],
+    "Culture & Festival": [
+        { word: "go hand in hand with...", translate: "與...關係密切", sentence: "Your health goes hand in hand with how often you exercise and what you eat every day. (你的健康與你多常運動以及每天吃什麼息息相關。)" },
+        { word: "get to", translate: "得以", sentence: "Ms. Chen got to take a vacation in Australia after she saved enough money. (陳女士存夠錢後得以去澳洲度假。)" },
+        { word: "take on", translate: "具有;呈現", sentence: "Many words take on different meanings over time. (許多字隨著時間呈現出不同的意義。)" },
+        { word: "present", translate: "現在(的);目前(的)", sentence: "Mr. Moore can't meet you at the present time because he's very busy. (摩爾先生目前無法見你，因為他很忙。)" }
+    ]
+};
